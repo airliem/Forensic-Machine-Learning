360e8bd46f2e76d596e4d855004ca0c0eb1836ab
Melissa Airlie <airliemelissa@gmail.com>
2021-12-29 21:02:28 +1000

    rename.py: add comments

--- rename.py
+++ rename.py
@@ -1,6 +1,9 @@
 import os
 
 # Set folderpath to the folder containing the images to rename
+# Takes a list of jpg files from folderpath and renames
+# them from original name to prefix_1.jpg .. prefix_n.jpg
+# where n is the number of images in the folderpath
 folderpath = r'$HOME/Documents/Basic_Hair_Modified/val/T3'
 prefix = 'T3'
 filenumber = 1
